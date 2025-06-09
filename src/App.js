1897fd4a71c9857f10d43f36e85bb3b14a52aee2
sepideh <sepideh0771@gmail.com>
2025-06-09 07:12:47 -0700

    Button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import * as React from "react";
-import { Typography as Type } from "@mui/material";
+import { Typography as Type,Button } from "@mui/material";
 
 function App() {
   return (
     <div>
-      <Type variant="h2" component={"h1"} sx={{color:'red'}}> Typography</Type>
+      <Type variant="h2" component={"h1"} sx={{color:'red'}} disabled> Typography</Type>
+      <Button variant="contained" color='success' sx={{margin:'30px'}}>Click Me</Button>
+            <Button variant="text">Click Me</Button>
+      <Button variant="outlined" onClick={()=>alert('you clicked')} size="large">Click Me</Button>
+
       <h1>MUI TUTORAILS</h1>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium
